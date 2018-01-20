26c1a96e2aece5e52600ee5d49361181eeee949c
Allen He <Balthazar@huntsman-ve502-0508.apn.wlan.upenn.edu>
2018-01-20 00:48:00 -0500

    Set up input container, input row, complete row components

--- backend/static/app.jsx
+++ backend/static/app.jsx
@@ -1,33 +1,67 @@
-function handleInput(){
-	console.log("here");
-}
-
 class InputRow extends React.Component {
 	render(){
 		return(
 			<div className="inputRow">
-				<input className="inputForm" onkeypress="handleInput"/>
+				<input className="inputForm" onKeyPress={this.props.handleInput}/>
+			</div>
+		)
+	}
+}
+
+class CompleteRow extends React.Component {
+	render(){
+		return(
+			<div className="completeRow">
+				{this.props.text}
 			</div>
 		)
 	}
 }
 
 class InputContainer extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			children: []
+		}
+	}
+
+	handleInput(e){
+		if(e.key == "Enter"){
+			console.log("new line");
+			this.setState({
+				children: this.state.children.concat([<CompleteRow text="child"/>])
+			});
+		} else if (e.key == " "){
+			this.props.getSuggestions("word")
+		}
+	}
+
 	render(){
+		console.log(this.props)
 		return(
 			<div className="inputContainer">
-				<InputRow/>
+				{this.state.children}
+				<InputRow handleInput={this.handleInput.bind(this)}/>
 			</div>
 		)
 	}
 }
 
 class App extends React.Component {
+	getSuggestions(word){
+		console.log(word);
+		$.post('/api/getsuggestions', {word: 'hello'}, function(suggestions){
+			console.log(suggestions);
+		})
+	}
+
 	render(){
 		return(
 			<div className="app">
 				<h1>Poetry and Me</h1>
-				<InputContainer/>
+				<InputContainer getSuggestions={this.getSuggestions.bind(this)}/>
 			</div>
 		)
 	}
