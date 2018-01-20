fe64b964b5e44b3d0150f620c8a673150b4cc9da
Deepan Saravanan <deepan.saravanan83@gmail.com>
2018-01-20 18:18:36 -0500

    finished backend call in app.py

---
+++ backend/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, render_template, jsonify
+from predictor import SonnetPredictor
+import spacy
+import sys
+
+sys.path.append("../scripts")
+from syllables import count_syllables
+
+model_path = "model path here"
+vocab_path = "vocab path here"
+sp = SonnetPredictor(model_path, vocab_path)
+
+current_sonnet = ""
+
+app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
+
+nlp = spacy.load('en')
+
+app.config['DEBUG'] = True
+
+@app.route('/')
+def main():
+	current_sonnet = ""
+	sp = SonnetPredictor(model_path, vocab_path)
+	return render_template('main.html')
+
+@app.route('/api/newquatrain', methods=['POST'])
+def new_quatrain():
+	sp.new_quatrain()
+	current_sonnet = ""
+
+
+@app.route('/api/getsuggestions', methods=['POST'])
+def get_suggestions():
+	word = request.form['word']
+	suggestions = getSuggestions(word)
+	return jsonify({'suggestions': suggestions})
+
+# this will be the backend call
+def getSuggestions(word):
+	if word == "\n":
+		current_sonnet = current_sonnet + " EOS"
+	else:
+		current_sonnet = current_sonnet + " " + word;
+
+	tokens = nlp(current_sonnet)
+	last_token = tokens[len(tokens) - 1]
+
+	token = last_token.text
+	pos_tag = last_token.pos_
+	suffix = token[-2:]
+	num_syllables = count_syllables(token)
+	return sp.add_word((token.lower(), pos_tag, suffix, num_syllables));
