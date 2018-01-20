0b84d773187a87fab3f6fbcf53950c4bab4a4933
John Hewitt <john.hewitt64@gmail.com>
2018-01-20 01:01:13 -0500

    add skeleton sonnet predictor

---
+++ backend/predictor.py
@@ -0,0 +1,22 @@
+from nn import RNNLM
+
+
+class SonnetPredictor:
+
+  def __init__(self):
+    pass
+
+  def load_RNNLM(self, param_path):
+    pass
+
+  def add_word(self, token):
+    pass
+
+  def new_quatrain(self):
+    pass
+
+  def delete_word(self):
+    pass
+
+
+
