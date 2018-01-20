af1c16731552e7ddf9ebd80dbd41de21b1cb29ac
John Hewitt <john.hewitt64@gmail.com>
2018-01-20 17:49:16 -0500

    add rnnlm prediction

--- backend/nn.py
+++ backend/nn.py
@@ -1,7 +1,9 @@
 import argparse
+from syllables import count_syllables
 import dynet as dy
 from vocab import Vocab, FIRST_TOK, SECOND_TOK, THIRD_TOK, FOURTH_TOK, EOQ_TOK
 import sys
+import json
 from attender import EmptyAttender
 import en_core_web_sm
 nlp = en_core_web_sm.load()
@@ -58,6 +60,7 @@ class RNNLM:
       toks_so_far = []
       token = [self.word_vocab[last_tok], self.pos_vocab[last_tok], self.suffix_vocab[last_tok], syll_count]
       while last_tok != EOQ_TOK and len(toks_so_far) < 3*15 :
+        #print([last_tok, last_pos, last_suffix, syll_count])
         token = [self.word_vocab[last_tok], self.pos_vocab[last_pos], self.suffix_vocab[last_suffix], syll_count]
 
         state, probs = self.add_input(state, token)
@@ -69,8 +72,8 @@ class RNNLM:
         toks_so_far.append(next_word_tok)
         last_tok = next_word_tok
         last_pos = parsed_string[-1].tag_
-        last_suffix = parsed_string[-1].text
-        syll_count = 0 #len(parsed_string[-1].text)/3
+        last_suffix = parsed_string[-1].text[-2:]
+        syll_count = count_syllables(last_tok)
       print(string_so_far)
 
   def add_input(self, state, token):
@@ -85,13 +88,17 @@ class RNNLM:
 
 class RNNLMTrainer:
 
-  def __init__(self, pc, epochs=30):
+  def __init__(self, pc, epochs=40):
     self.epochs = epochs
     self.pc = pc
-    self.trainer = dy.AdamTrainer(pc)
+    self.trainer = dy.AdamTrainer(pc, alpha=.005)
     self.BATCH_SIZE = 1
+    self.lr = .005
 
-  def get_vocabs(self, quatrains):
+  def load_vocabs(self, vocabs_path):
+    return json.load(open(vocabs_path))
+
+  def get_vocabs(self, quatrains, vocabs_path):
     word_vocab = Vocab()
     pos_vocab = Vocab()
     suffix_vocab = Vocab()
@@ -100,6 +107,7 @@ class RNNLMTrainer:
         word_vocab.add(word)
         pos_vocab.add(pos)
         suffix_vocab.add(suffix)
+    Vocab.vocabs_to_file([word_vocab, pos_vocab, suffix_vocab], vocabs_path)
     return word_vocab, pos_vocab, suffix_vocab
 
   def symbol_augment_quatrain(self, quatrain, quatrain_index):
@@ -116,7 +124,6 @@ class RNNLMTrainer:
         quatrains.append(quatrain)
         quatrain_index+= 1
         quatrain_index = quatrain_index  if quatrain_index < 4 else 0
-        print(quatrain)
     return quatrains
 
   def integerize_quatrains(self, vocabs, quatrains):
@@ -127,46 +134,66 @@ class RNNLMTrainer:
       for word, pos, suffix, num_syll in quatrain:
         integerized_tokens.append((word_vocab[word], pos_vocab[pos], suffix_vocab[suffix], num_syll))
       integerized_quatrains.append(integerized_tokens)
-      #print(integerized_tokens)
     return integerized_quatrains
 
-  def train(self, rnnlm, quatrains):
-    min_loss = sys.maxsize
+  def train(self, rnnlm, train_quatrains, dev_quatrains):
+    min_dev_loss = sys.maxsize
     for i in tqdm(range(self.epochs), desc='Training'):
+
       losses = []
       tqdm.write('Epoch {}'.format(i))
       total_loss = 0
       state = rnnlm.initialize()
-      for count, quatrain in enumerate(quatrains):
-        #print(count, quatrain[0])
+
+      for count, quatrain in enumerate(train_quatrains):
         for token, (next_word, _, _, _) in zip(quatrain, quatrain[1:]):
           state, probs = rnnlm.add_input(state, token)
           loss = -dy.log(dy.pick(probs, next_word))
           losses.append(loss)
+
         if count % self.BATCH_SIZE == 0:
           loss = dy.esum(losses)
           total_loss += loss.value()
           loss.backward()
           self.trainer.update()
           losses = []
-          #dy.renew_cg()
-          #state = rnnlm.initialize()
+
         if (count + 1)% 4 == 0:
           dy.renew_cg()
           state = rnnlm.initialize()
+
+      dev_loss = 0
+      state = rnnlm.initialize()
+      for count, quatrain in enumerate(dev_quatrains):
+        for token, (next_word, _, _, _) in zip(quatrain, quatrain[1:]):
+          state, probs = rnnlm.add_input(state, token)
+          loss = -dy.log(dy.pick(probs, next_word))
+          dev_loss += loss.value()
+        if (count + 1)% 4 == 0:
+          dy.renew_cg()
+          state = rnnlm.initialize()
+      print('Dev loss: {}'.format(dev_loss))
+      if dev_loss < min_dev_loss:
+        tqdm.write('Best dev loss. Saving parameters...')
+        self.pc.save('model.pt')
+        min_loss = dev_loss
+      else:
+        tqdm.write('Not brst dev loss. Restarting with smaller...')
+        self.lr = self.lr * .5
+        self.trainer.restart(self.lr)
+
       tqdm.write('Training Loss: {}'.format(total_loss))
       rnnlm.generate(rnnlm.initialize())
-      if total_loss < min_loss:
-        min_loss = total_loss
 
 if __name__ == '__main__':
   argp = argparse.ArgumentParser()
   argp.add_argument('quatrains_filepath')
+  argp.add_argument('vocabs_filepath')
   args = argp.parse_args()
   pc = dy.ParameterCollection()
   model_trainer = RNNLMTrainer(pc)
   quatrains = model_trainer.load_raw_quatrains(args.quatrains_filepath)
-  vocabs = model_trainer.get_vocabs(quatrains)
+  vocabs = model_trainer.get_vocabs(quatrains, args.vocabs_filepath)
   integerized_quatrains = model_trainer.integerize_quatrains(vocabs, quatrains)
   rnnlm = RNNLM(vocabs, pc)
-  model_trainer.train(rnnlm, integerized_quatrains)
+  model_trainer.train(rnnlm, integerized_quatrains[:-16], integerized_quatrains[-16:])
--- backend/vocab.py
+++ backend/vocab.py
@@ -1,3 +1,5 @@
+import json
+
 FIRST_TOK = '__FIRST'
 SECOND_TOK = '__SECOND'
 THIRD_TOK = '__THIRD'
@@ -15,6 +17,31 @@ class Vocab:
     self.add(THIRD_TOK)
     self.add(FOURTH_TOK)
     self.add(UNK_TOK)
+
+  @staticmethod
+  def vocabs_from_file(filepath):
+    word_data, pos_data, suffix_data = json.load(open(filepath))
+    word_vocab = Vocab()
+    word_vocab.sym_to_int, word_vocab.int_to_sym = word_data
+    word_vocab.int_to_sym = {int(c):word_vocab.int_to_sym[c] for c in word_vocab.int_to_sym}
+    pos_vocab = Vocab()
+    pos_vocab.sym_to_int, pos_vocab.int_to_sym = pos_data
+    pos_vocab.int_to_sym = {int(c):pos_vocab.int_to_sym[c] for c in pos_vocab.int_to_sym}
+    suffix_vocab = Vocab()
+    suffix_vocab.sym_to_int, suffix_vocab.int_to_sym = suffix_data
+    suffix_vocab.int_to_sym = {int(c):suffix_vocab.int_to_sym[c] for c in suffix_vocab.int_to_sym}
+    return word_vocab, pos_vocab, suffix_vocab
+
+  @staticmethod
+  def vocabs_to_file(vocabs, filepath):
+    word_vocab, pos_vocab, suffix_vocab = vocabs
+    print(word_vocab.int_to_sym)
+    json.dump(
+        [[word_vocab.sym_to_int, word_vocab.int_to_sym],
+          [pos_vocab.sym_to_int, pos_vocab.int_to_sym],
+          [suffix_vocab.sym_to_int, suffix_vocab.int_to_sym]
+        ], open(filepath, 'w'))
+
 
   def __getitem__(self, obj):
     if obj in self.sym_to_int:
