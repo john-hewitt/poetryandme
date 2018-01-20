7747357e244654f6b9dd440f7d35d7e1c956280c
John Hewitt <john.hewitt64@gmail.com>
2018-01-20 00:57:35 -0500

    working rnnlm implementation almost ready to be trained

--- backend/nn.py
+++ backend/nn.py
@@ -1,30 +1,93 @@
 import argparse
 import dynet as dy
-from vocab import Vocab
+from vocab import Vocab, FIRST_TOK, SECOND_TOK, THIRD_TOK, FOURTH_TOK, EOQ_TOK
+import sys
+from attender import EmptyAttender
+import en_core_web_sm
+nlp = en_core_web_sm.load()
 import yaml
 import json
+import spacy
+from tqdm import tqdm
 
 
 class RNNLM:
 
-  def __init__(self, vocabs):
+  def __init__(self, vocabs, pc):
     self.word_vocab, self.pos_vocab, self.suffix_vocab = vocabs
 
-    self.pc = dy.ParameterCollection()
+    self.pc = pc
+    self.WORD_EMBED_SIZE = 100
+    self.POS_EMBED_SIZE = 20
+    self.SUFFIX_EMBED_SIZE = 40
+    self.SYLL_SIZE = 1
+    self.total_input_size = self.WORD_EMBED_SIZE + self.POS_EMBED_SIZE \
+        + self.SUFFIX_EMBED_SIZE + self.SYLL_SIZE
 
-  def one_epcoh():
-    pass
+    self.RNN_HIDDEN_SIZE = 80
 
-  def loss():
-    pass
+    self.rnn = dy.VanillaLSTMBuilder(1, self.total_input_size, self.RNN_HIDDEN_SIZE, pc)
 
-  def topks():
-    pass
+    self.attender = EmptyAttender()
+
+    self.word_vecs = self.pc.add_lookup_parameters((len(self.word_vocab), self.WORD_EMBED_SIZE))
+    self.pos_vecs = self.pc.add_lookup_parameters((len(self.pos_vocab), self.POS_EMBED_SIZE))
+    self.suffix_vecs = self.pc.add_lookup_parameters((len(self.suffix_vocab), self.SUFFIX_EMBED_SIZE))
+
+    self.W = self.pc.add_parameters((len(self.word_vocab), self.RNN_HIDDEN_SIZE))
+    self.b = self.pc.add_parameters((len(self.word_vocab)))
+
+  def initialize(self):
+    return self.rnn.initial_state()
+
+  def get_concatenated_representation(self, token):
+    word, pos, suffix, syll_count = token
+    word_vec = self.word_vecs[word]
+    pos_vec = self.pos_vecs[pos]
+    suffix_vec = self.suffix_vecs[suffix]
+    syll_vec = dy.inputVector([syll_count])
+    return dy.concatenate([word_vec, pos_vec, suffix_vec, syll_vec])
+
+  def generate(self, state):
+    for last_tok in [FIRST_TOK, SECOND_TOK, THIRD_TOK, FOURTH_TOK]:
+      last_pos = last_tok
+      last_suffix = last_tok
+      syll_count = 0
+      string_so_far = ''
+      toks_so_far = []
+      while last_tok != EOQ_TOK and len(toks_so_far) < 15:
+        token = [self.word_vocab[last_tok], self.pos_vocab[last_tok], self.suffix_vocab[last_tok], syll_count]
+
+        state, probs = self.add_input(state, token)
+        probs = probs.value()
+        next_word_tok = self.word_vocab[probs.index(max(probs))]
+
+        string_so_far += next_word_tok + ' '
+        parsed_string = nlp(string_so_far)
+        toks_so_far.append(next_word_tok)
+        last_tok = next_word_tok
+        last_pos = parsed_string[-1].tag_
+        last_suffix = parsed_string[-1].text
+        syll_count = 0 #len(parsed_string[-1].text)/3
+      print(string_so_far)
+
+  def add_input(self, state, token):
+    input_vec = self.get_concatenated_representation(token)
+    hidden_state = state.add_input(input_vec)
+    prediction_vector = hidden_state.output()
+    b = dy.parameter(self.b)
+    W = dy.parameter(self.W)
+
+    probs = dy.softmax(dy.affine_transform([b, W, prediction_vector]))
+    return hidden_state, probs
 
 class RNNLMTrainer:
 
-  def __init__(self, epochs = 30):
+  def __init__(self, pc, epochs=30):
     self.epochs = epochs
+    self.pc = pc
+    self.trainer = dy.AdamTrainer(pc)
+    self.BATCH_SIZE = 1
 
   def get_vocabs(self, quatrains):
     word_vocab = Vocab()
@@ -37,24 +100,65 @@ class RNNLMTrainer:
         suffix_vocab.add(suffix)
     return word_vocab, pos_vocab, suffix_vocab
 
+  def symbol_augment_quatrain(self, quatrain, quatrain_index):
+    augment_symbols = [FIRST_TOK, SECOND_TOK, THIRD_TOK, FOURTH_TOK]
+    return [[augment_symbols[quatrain_index]]*3 + [0] ] + quatrain + [[EOQ_TOK]*3 + [0]]
+  
   def load_raw_quatrains(self, quatrains_filepath):
     quatrains = []
+    quatrain_index = 0
     for line in open(quatrains_filepath):
       if line.strip():
-        quatrains.append(json.loads(line.strip()))
+        quatrain = self.symbol_augment_quatrain(
+            json.loads(line.strip()), quatrain_index)
+        quatrains.append(quatrain)
+        quatrain_index = quatrain_index +1 if quatrain_index < 3 else 0
+        print(quatrain)
     return quatrains
 
-  def train():
-    for i in range(self.epochs):
-      pass
+  def integerize_quatrains(self, vocabs, quatrains):
+    word_vocab, pos_vocab, suffix_vocab = vocabs
+    integerized_quatrains = []
+    for quatrain in quatrains:
+      integerized_tokens = []
+      for word, pos, suffix, num_syll in quatrain:
+        integerized_tokens.append((word_vocab[word], pos_vocab[pos], suffix_vocab[suffix], num_syll))
+      integerized_quatrains.append(integerized_tokens)
+      print(integerized_tokens)
+    return integerized_quatrains
 
+  def train(self, rnnlm, quatrains):
+    min_loss = sys.maxsize
+    for i in tqdm(range(self.epochs), desc='Training'):
+      losses = []
+      tqdm.write('Epoch {}'.format(i))
+      total_loss = 0
+      rnnlm.generate(rnnlm.initialize())
+      for count, quatrain in enumerate(quatrains):
+        state = rnnlm.initialize()
+        for token, (next_word, _, _, _) in zip(quatrain, quatrain[1:]):
+          state, probs = rnnlm.add_input(state, token)
+          loss = -dy.log(dy.pick(probs, next_word))
+          losses.append(loss)
+        if count % self.BATCH_SIZE == 0:
+          loss = dy.esum(losses)
+          total_loss += loss.value()
+          loss.backward()
+          self.trainer.update()
+          dy.renew_cg()
+          losses = []
+      tqdm.write('Training Loss: {}'.format(total_loss))
+      if total_loss < min_loss:
+        min_loss = total_loss
 
 if __name__ == '__main__':
   argp = argparse.ArgumentParser()
   argp.add_argument('quatrains_filepath')
   args = argp.parse_args()
-  model_trainer = RNNLMTrainer()
+  pc = dy.ParameterCollection()
+  model_trainer = RNNLMTrainer(pc)
   quatrains = model_trainer.load_raw_quatrains(args.quatrains_filepath)
   vocabs = model_trainer.get_vocabs(quatrains)
-  print(vocabs)
-
+  integerized_quatrains = model_trainer.integerize_quatrains(vocabs, quatrains)
+  rnnlm = RNNLM(vocabs, pc)
+  model_trainer.train(rnnlm, integerized_quatrains)
--- backend/vocab.py
+++ backend/vocab.py
@@ -1,23 +1,36 @@
+FIRST_TOK = '__FIRST'
+SECOND_TOK = '__SECOND'
+THIRD_TOK = '__THIRD'
+FOURTH_TOK = '__FOURTH'
+UNK_TOK = '__UNK'
+EOQ_TOK = '</quatrain>'
+
 class Vocab:
 
   def __init__(self):
     self.sym_to_int = {}
     self.int_to_sym = {}
-    self.add('__FIRST')
-    self.add('__SECOND')
-    self.add('__THIRD')
-    self.add('__FOURTH')
+    self.add(FIRST_TOK)
+    self.add(SECOND_TOK)
+    self.add(THIRD_TOK)
+    self.add(FOURTH_TOK)
+    self.add(UNK_TOK)
 
   def __getitem__(self, obj):
     if obj in self.sym_to_int:
       return self.sym_to_int[obj]
     elif obj in self.int_to_sym:
       return self.int_to_sym[obj]
+    elif isinstance(obj, str):
+      return self.sym_to_int[UNK_TOK]
     else:
-      raise Exception("Symbol not found")
+      raise Exception("Int not found")
 
   def __repr__(self):
     return str(self.sym_to_int)
+
+  def __len__(self):
+    return len(self.sym_to_int)
 
   def add(self, obj):
     if obj not in self.sym_to_int:
